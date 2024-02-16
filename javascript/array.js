504c01452a522797d28d60f92bfe2c9ff3272423
Mark-shija <plusmaxtechtz@gmail.com>
2024-02-16 12:50:21 +0300

    Javascript Dom

---
+++ javascript/array.js
@@ -0,0 +1,6 @@
+var student = document.getElementById("header");
+
+student.innerHTML = " I was changed by Javascript DOM";
+const ul = document.querySelector('.items');
+ul.firstElementChild.textContent = "Good afternoon";
+ul.children[1].innerText = "Second element in list";
